57415541c128c20cb0b322c03d73f88ac85524fb
Tatsuro <tatsurohida@gmail.com>
2018-06-10 20:17:15 +0900

    create php

---
+++ index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title>アメリカ</title>
+    <script type="text/javascript" src="america.js"></script>
+    <style>
+      #canvas{
+        background:#fff;
+        margin:10px, 10px, 10px, 10px;
+        width: 100%;
+      }
+    </style>
+  </head>
+  <body>
+	<div class="upload"><input type="file" name="file" id="file"></div>
+    <canvas id="canvas"></canvas>
+  </body>
+</html>
